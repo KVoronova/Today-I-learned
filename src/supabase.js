02043ebe73349569b7176e4ecc993397b24f537c
Ksenia Voronova <48063501+KVoronova@users.noreply.github.com>
2024-12-19 20:04:33 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://kgwssgkshlersikwhrsq.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtnd3NzZ2tzaGxlcnNpa3docnNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExMjA4MjEsImV4cCI6MjAyNjY5NjgyMX0.MTY_EISEN2Xna_Nws-TqJrEplJP1S0CaEXjqOVfmcWc";
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://gkrhfmhgdtdrjxrbzgqm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrcmhmbWhnZHRkcmp4cmJ6Z3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwMjkzNjMsImV4cCI6MjA0OTYwNTM2M30.D-rQ8Gs0KE7pRzUYFQRElnH3kYUm66yQ1Br-5_Ue3UM';
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
